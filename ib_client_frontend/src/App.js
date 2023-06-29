4fc7db812c09d260d22bfb4fa10ca2ed5ad412a4
Michael Rogers <michael@estatespace.com>
2023-06-29 08:42:27 -0700

    added store to Routes in app.js

--- ib_client_frontend/src/App.js
+++ ib_client_frontend/src/App.js
@@ -1,16 +1,17 @@
 import "./App.css";
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 import Gallery from "./pages/gallery/Gallery";
 import Profile from "./pages/profile/Profile";
+import Store from "./pages/store/store";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path ='/' element={<div>Landing PAge</div>} />
-        <Route path ='/gallery' element={<Gallery />} />
-        <Route path ='/profile' element={<Profile /> }/>
-        
+        <Route path="/" element={<div>Landing PAge</div>} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/store" element={<Store />} />
       </Routes>
     </div>
   );
