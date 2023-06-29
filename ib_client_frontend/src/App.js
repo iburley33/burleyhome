73b75d5e120a1b63be0be6a64dc700987a9bfdf5
iburley33 <iburley33@gmail.com>
2023-06-29 11:32:14 -0400

    RRD installed

--- ib_client_frontend/src/App.js
+++ ib_client_frontend/src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
+import {Routes, Route} from 'react-router-dom';
+import Gallery from "./pages/gallery/Gallery";
+import Profile from "./pages/profile/Profile";
 
 function App() {
   return (
     <div className="App">
-      <header>header</header>
-      <div>page</div>
-      <footer>footer</footer>
+      <Routes>
+        <Route path ='/' element={<div>Landing PAge</div>} />
+        <Route path ='/gallery' element={<Gallery />} />
+        <Route path ='/profile' element={<Profile /> }/>
+        
+      </Routes>
     </div>
   );
 }
