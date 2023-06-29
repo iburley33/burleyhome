49a4ee9aec5fe56bf64959b7516bfd44f1e6b9c7
Michael Rogers <michael@estatespace.com>
2023-06-28 17:30:38 -0700

    initial commit

---
+++ ib_client_frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header>header</header>
+      <div>page</div>
+      <footer>footer</footer>
+    </div>
+  );
+}
+
+export default App;
