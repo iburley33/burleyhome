22051620993406d7127fa8946333eac6dfed9f36
iburley33 <iburley33@gmail.com>
2023-07-17 15:07:50 -0400

    dark slider working

--- ib_client_frontend/src/components/darkcard/DarkCard.jsx
+++ ib_client_frontend/src/components/darkcard/DarkCard.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import './darkcardcss.css';
-import { Slide } from 'react-slideshow-image';
-import 'react-slideshow-image/dist/styles.css';
-import portrait from '../../assets/imgs/portrait-PH.jpeg';
+import ThreeFrame from '../threeframe/ThreeFrame';
+
 
 function DarkCard({ image, img1, img2, img3, img4, img5, title, description, link, link1, link2, link3, link4, link5, detail }) {
-  console.log({ image, img1, img2, img3, img4, img5, title, description, link,link1, link2, link3, link4, link5, detail });
- 
-  const images = [
-    portrait,
-    portrait,
-    portrait,
-  ];
+
+
 
   return (
     <div className='darkCard'>
@@ -22,23 +16,7 @@ function DarkCard({ image, img1, img2, img3, img4, img5, title, description, lin
         <h5 class='darkDetail'>{detail}</h5>
       </div>
       <div className='darkImgDiv'>
-      <Slide slidesToScroll={1} slidesToShow={3} indicators={true}>
-            <div className="each-slide-effect">
-                <div style={{ 'backgroundImage': `url(${images[0]})` }}>
-                    <span>Slide 1</span>
-                </div>
-            </div>
-            <div className="each-slide-effect">
-                <div style={{ 'backgroundImage': `url(${images[1]})` }}>
-                    <span>Slide 2</span>
-                </div>
-            </div>
-            <div className="each-slide-effect">
-                <div style={{ 'backgroundImage': `url(${images[2]})` }}>
-                    <span>Slide 3</span>
-                </div>
-            </div>
-        </Slide>
+        <ThreeFrame />
       </div>
     </div>
   );
