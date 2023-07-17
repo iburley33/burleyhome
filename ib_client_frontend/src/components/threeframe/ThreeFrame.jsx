cc7d86c5241ad168b475cb6c672717c502187d1b
iburley33 <iburley33@gmail.com>
2023-07-17 16:16:35 -0400

    dark card looking good

--- ib_client_frontend/src/components/threeframe/ThreeFrame.jsx
+++ ib_client_frontend/src/components/threeframe/ThreeFrame.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "./threeframecss.css"
+import "./threeframecss.css";
+import image1 from '../../assets/imgs/1.jpg';
+import image2 from '../../assets/imgs/2.jpg';
+import image3 from '../../assets/imgs/3.jpg';
+import image4 from '../../assets/imgs/4.jpg';
+import image5 from '../../assets/imgs/5.jpg';
+import image6 from '../../assets/imgs/6.jpg';
 
 
 export default function ThreeFrame() {
@@ -16,24 +22,25 @@ export default function ThreeFrame() {
   };
   return (
     <Slider {...settings}>
-      <div>
-        <h3>1</h3>
+      <div className="item">
+        <img alt="" src={image1}></img>
       </div>
-      <div>
-        <h3>2</h3>
+      <div className="item">
+        <img alt="" src={image2}></img>
       </div>
-      <div>
-        <h3>3</h3>
+      <div className="item">
+        <img alt="" src={image3}></img>
       </div>
-      <div>
-        <h3>4</h3>
+      <div className="item">
+        <img alt="" src={image4}></img>
       </div>
-      <div>
-        <h3>5</h3>
+      <div className="item">
+        <img alt="" src={image5}></img>
       </div>
-      <div>
-        <h3>6</h3>
+      <div className="item">
+        <img alt="" src={image6}></img>
       </div>
+
     </Slider>
   );
 }
