1b4d9721c2ca44c485827929a4e6015a8a75054e
iburley33 <iburley33@gmail.com>
2023-07-29 16:18:13 -0400

    all requests complete

--- ib_client_frontend/src/components/slider/slider.jsx
+++ ib_client_frontend/src/components/slider/slider.jsx
@@ -1,15 +1,23 @@
 import './slidercss.css';
 import { motion } from 'framer-motion';
 import { useRef, useEffect, useState } from 'react';
-import images from './sliderImages';
+import image1 from '../../assets/imgs/1.jpg';
+import image2 from '../../assets/imgs/2.jpg';
+import image3 from '../../assets/imgs/3.jpg';
+import image4 from '../../assets/imgs/4.jpg';
+import image5 from '../../assets/imgs/5.jpg';
+import image6 from '../../assets/imgs/1.jpg';
 
 function Slider() {
+
   const [ width, setWidth] = useState(0);
   const carousel = useRef();
 
   useEffect(() => {
     setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth)
   }, []);
+  
+  const images  = [image1, image2, image3, image4, image5, image6];
 
   return (
     <div className="App">
