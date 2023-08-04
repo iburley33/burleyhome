a61d950e7e569d45b3626c44444a3736fd1c7d68
iburley33 <iburley33@gmail.com>
2023-08-04 14:53:18 -0400

    fixed store name

---
+++ ib_client_frontend/src/pages/store/Store.jsx
@@ -0,0 +1,10 @@
+/* Framework tools & CSS */
+import React from "react";
+import "./profilecss.css";
+import Subhead from "../../components/subhead/Subhead";
+
+export default function Store() {
+  return(
+    <Subhead pageName='Shop'/>
+  );
+}
